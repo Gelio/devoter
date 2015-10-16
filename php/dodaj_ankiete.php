07788c76cb6c4aef5ebbd08741039d6e507f576b
Sarkerq <kosma1996@gmail.com>
2015-10-16 20:24:38 +0200

    Add poll - first draft

--- php/dodaj_ankiete.php
+++ php/dodaj_ankiete.php
@@ -1,17 +1,53 @@
-<?php
+ï»¿<?php
 $nazwa = $_POST['name'];
 $opcje = $_POST['options'];
-for($i=0;$i<=$opcje;$i++)
+$prywatne = $_POST['private'];
+$czas_trwania=$_POST['expDate'];
+try
+   {
+      $pdo = new PDO('mysql:host=localhost;dbname=devot;charset=utf8', 'root', '', array(PDO::ATTR_EMULATE_PREPARES => false,PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+  
+
+$stmt=$pdo->query('SELECT id FROM polls') ;
+$id= $stmt->rowCount()+1;
+$stmt = $pdo -> prepare('INSERT INTO `polls` (`id`,  `name`, `total_votes`,`private`,`expire_date`)	VALUES(
+				:id,
+				:name,
+				:total_votes,
+				:private,
+				:expire_date)');
+				$stmt -> bindValue(':id', $id, PDO::PARAM_INT); 
+			$stmt -> bindValue(':name', $nazwa, PDO::PARAM_STR);
+			$stmt -> bindValue(':total_votes',0 , PDO::PARAM_INT);
+			$stmt -> bindValue(':private', $prywatne, PDO::PARAM_INT);
+			$stmt -> bindValue(':expDate', $czas_trwania, PDO::PARAM_LOB);
+			$stmt -> execute();
+
+$stmt=$pdo->query('SELECT id FROM options');
+$oid= $stmt->rowCount()+1;
+foreach($opcje as $obecna)
 {
-$op_nazw[i]=$_POST["opname_$i"];
+	$stmt = $pdo -> prepare('INSERT INTO `options` (`id`,  `poll_id`, `name`,`amount`)	VALUES(
+				:id,
+				:poll_id,
+				:name,
+				:amount)');
+				$stmt -> bindValue(':id', $oid, PDO::PARAM_INT); 
+			$stmt -> bindValue(':poll_id', $id, PDO::PARAM_INT);
+			$stmt -> bindValue(':name',$obecna , PDO::PARAM_STR);
+			$stmt -> bindValue(':amount', 0, PDO::PARAM_INT);
+	
+			$stmt -> execute();
+	
+	$oid++;
+	
 }
-//kod sanituj¹cy - php pod
-//po³¹cz z baz¹
-insert into polls values($id,$nazwa,0,$date);
-$id++;
-for($i=0;$i<=$opcje;$i++)
-{
-insert into options values($id,$oid,$op_nazw[i],0);
-$oid++;
-}
+echo "Ankieta zostaÅ‚a utworzona. </br> ID ankiety wynosi $id";
+   }
+    
+   catch(PDOException $e)
+   {
+      echo 'PoÅ‚Ä…czenie nie mogÅ‚o zostaÄ‡ utworzone: ' . $e->getMessage();
+   }
+
 ?>
