9d12dcb9b76059d6acaef1f1a486194c83edbf40
Gelio <voreny.gelio@gmail.com>
2015-10-16 23:56:41 +0200

    added a template config file for PHP scripts

---
+++ php/config-template.inc.php
@@ -0,0 +1,11 @@
+<?php
+// Rename this file to config.inc.php and include it in your PHP scripts
+
+$database["ip"] = "localhost";
+$database["user"] = "root";
+$database["password"] = "";
+$database["port"] = 3306;
+
+$database["db"] = "devoter";
+
+?>
