cb391e70e032be33b55ba008ee562be86ce58c88
Sarkerq <kosma1996@gmail.com>
2015-10-18 21:28:35 +0200

    Polished few mistakes, organized code, softcoded database connect, added voting - first draft

--- php/dodaj_ankiete.php
+++ php/dodaj_ankiete.php
@@ -1,53 +1,60 @@
 ﻿<?php
-$nazwa = $_POST['name'];
-$opcje = $_POST['options'];
-$prywatne = $_POST['private'];
-$czas_trwania=$_POST['expDate'];
+	include 'config.inc.php';
+	$nazwa = $_POST['name'];
+	$opcje = $_POST['options'];
+	$prywatne = $_POST['private'];
+	$czas_trwania=$_POST['expDate'];
+
+	
 try
-   {
-      $pdo = new PDO('mysql:host=localhost;dbname=devot;charset=utf8', 'root', '', array(PDO::ATTR_EMULATE_PREPARES => false,PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+{
+      $pdo = new PDO("mysql:host=$database["ip"] ;dbname=$database["db"] ;port=$database["port"];charset=utf8, $database["user"] , $database["password"] ", array(PDO::ATTR_EMULATE_PREPARES => false,PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
   
 
-$stmt=$pdo->query('SELECT id FROM polls') ;
-$id= $stmt->rowCount()+1;
-$stmt = $pdo -> prepare('INSERT INTO `polls` (`id`,  `name`, `total_votes`,`private`,`expire_date`)	VALUES(
-				:id,
-				:name,
-				:total_votes,
-				:private,
-				:expire_date)');
-				$stmt -> bindValue(':id', $id, PDO::PARAM_INT); 
-			$stmt -> bindValue(':name', $nazwa, PDO::PARAM_STR);
-			$stmt -> bindValue(':total_votes',0 , PDO::PARAM_INT);
-			$stmt -> bindValue(':private', $prywatne, PDO::PARAM_INT);
-			$stmt -> bindValue(':expDate', $czas_trwania, PDO::PARAM_LOB);
-			$stmt -> execute();
+	$stmt=$pdo->query('SELECT id FROM polls') ;
+	$id= $stmt->rowCount()+1;
 
-$stmt=$pdo->query('SELECT id FROM options');
-$oid= $stmt->rowCount()+1;
-foreach($opcje as $obecna)
-{
-	$stmt = $pdo -> prepare('INSERT INTO `options` (`id`,  `poll_id`, `name`,`amount`)	VALUES(
+	$stmt = $pdo -> prepare('INSERT INTO `polls` (`id`,  `name`, `total_votes`,`private`,`expire_date`)	VALUES(
+			:id,
+			:name,
+			:total_votes,
+			:private,
+			:expire_date)');
+		$stmt -> bindValue(':id', $id, PDO::PARAM_INT); 
+		$stmt -> bindValue(':name', $nazwa, PDO::PARAM_STR);
+		$stmt -> bindValue(':total_votes',0 , PDO::PARAM_INT);
+		$stmt -> bindValue(':private', $prywatne, PDO::PARAM_INT);
+		$stmt -> bindValue(':expDate', $czas_trwania, PDO::PARAM_LOB);
+		$stmt -> execute();
+
+	$stmt=$pdo->query('SELECT id FROM options');
+	$oid= $stmt->rowCount()+1;
+
+	
+	foreach($opcje as $obecna)
+	{
+		$stmt = $pdo -> prepare('INSERT INTO `options` (`id`,  `poll_id`, `name`,`amount`)	VALUES(
 				:id,
 				:poll_id,
 				:name,
 				:amount)');
-				$stmt -> bindValue(':id', $oid, PDO::PARAM_INT); 
+			$stmt -> bindValue(':id', $oid, PDO::PARAM_INT); 
 			$stmt -> bindValue(':poll_id', $id, PDO::PARAM_INT);
 			$stmt -> bindValue(':name',$obecna , PDO::PARAM_STR);
 			$stmt -> bindValue(':amount', 0, PDO::PARAM_INT);
-	
 			$stmt -> execute();
 	
-	$oid++;
+		$oid++;
 	
+	}
+
+	echo json_encode($id);
 }
-echo "Ankieta została utworzona. </br> ID ankiety wynosi $id";
-   }
     
-   catch(PDOException $e)
-   {
+catch(PDOException $e)
+{
       echo 'Połączenie nie mogło zostać utworzone: ' . $e->getMessage();
-   }
+	  echo json_encode($e);
+}
 
 ?>
--- php/glosuj.php
+++ php/glosuj.php
@@ -1,12 +1,53 @@
-Vote
+ï»¿
 <?php
-$ip = $_POST['IP'];
-//po³¹cz z baz¹
-$opcja =$_POST['option'];
-$ankieta=$_POST['poll'];
-if(!select count * from ip_voted where poll_id=$ankieta and IP=$ip)
-update polls set total_votes=total_votes+1 where id=$ankieta;
-update options set answers=answers+1 where id=$opcja;
-insert into ip_voted values($idv,$ankieta,$ip);
-$idv++
+	include 'config.inc.php';
+
+	$ip = getenv('HTTP_CLIENT_IP')?:
+	getenv('HTTP_X_FORWARDED_FOR')?:
+	getenv('HTTP_X_FORWARDED')?:
+	getenv('HTTP_FORWARDED_FOR')?:
+	getenv('HTTP_FORWARDED')?:
+	getenv('REMOTE_ADDR');
+
+if (filter_var($ip, FILTER_VALIDATE_IP))
+{
+	try
+   {
+		$pdo = new PDO("mysql:host=$database["ip"] ;dbname=$database["db"] ;port=$database["port"];charset=utf8, $database["user"] , $database["password"] ", array(PDO::ATTR_EMULATE_PREPARES => false,PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+		
+		$opcja =$_POST['optionID'];
+		$ankieta=$_POST['id'];
+		$stmt= $pdo -> query("SELECT * FROM ip_voted WHERE poll_id=$ankieta AND IP=$ip");
+		$ilosc=$stmt->rowCount();
+		
+		$stmt= $pdo -> query("SELECT * FROM ip_voted");
+		$idv=$stmt->rowCount()+1;
+		
+		if(!$ilosc)
+		{
+			$stmt= $pdo -> exec("insert into ip_voted values($idv,$ankieta,$ip)");
+			$stmt= $pdo -> exec("update polls set total_votes=total_votes+1 where id=$ankieta");
+			$stmt =$pdo -> exec("update options set answers=answers+1 where id=$opcja;")
+			$idv++;
+		}
+		else 
+		{
+			echo "You already DeVoted for some option!";
+		}
+	}
+	catch(PDOException $e)
+   {
+      echo 'PoÅ‚Ä…czenie nie mogÅ‚o zostaÄ‡ utworzone: ' . $e->getMessage();
+	  echo json_encode($e);
+   }
+
+}
+else
+{
+	$erro="Your IP couldn't be validated!";
+	
+	echo  $erro;
+	echo json_encode($erro);
+	
+}
 ?>
