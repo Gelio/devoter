3d780750e9e7b06987f9e3420f48bf29811b9c60
Voreny <voreny.gelio@gmail.com>
2015-10-19 19:04:29 +0200

    fix front-back end integration

--- assets/js/directives/popular-polls.js
+++ assets/js/directives/popular-polls.js
@@ -1,14 +1,14 @@
 var popularPollsModule = angular.module('popularPolls', ['parserModule']);
 
 popularPollsModule.controller('popularPollsCtrl', ['$scope', 'PollParser', function($scope, PollParser) {
-    
+
     $scope.polls = [];
     PollParser.fetchPolls('php/all-polls.php', function(data) {
         $scope.polls = data;
     }, function(response) {
         // Error already printed
         // TODO: show error message
-    }, {startFrom: 0, limitTo: 20}, false);
+    }, {startFrom: 1, limitTo: 20}, false);
 }]);
 
 popularPollsModule.directive('popularPolls', function() {
