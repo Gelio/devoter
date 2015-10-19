715f7137adada0e8ae20acf0736baa7684e385d0
Sarkerq <kosma1996@gmail.com>
2015-10-19 21:21:19 +0200

    Adding poll works now!

--- php/dodaj_ankiete.php
+++ php/dodaj_ankiete.php
@@ -3,6 +3,7 @@
 	if ($_SERVER['REQUEST_METHOD'] == 'POST' && empty($_POST)) $_POST = json_decode(file_get_contents('php://input'), true);
 	$name = $_POST['name'];
 	$option_number = $_POST['options'];
+	
 	$private = $_POST['private'];
 	$expire_date=$_POST['expDate'];
 
@@ -10,28 +11,31 @@
 try
 {
      $pdo = new PDO("mysql:host=".$database['ip'].";
-	  dbname=".$database['db']." ;port=".$database['port'].";
-	  charset=utf8,".$database['user'].",".$database['password'] ,
+	  dbname=".$database['db'].";port=".$database['port'].";
+	  encoding=utf8",$database['user'],$database['password'] ,
 	  array(PDO::ATTR_EMULATE_PREPARES => false,
 	  PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
-  
+	 
 
 	$stmt=$pdo->query('SELECT id FROM polls;') ;
 	$id= $stmt->rowCount()+1;
 
-	$stmt = $pdo -> prepare('INSERT INTO `polls` (`id`,  `name`, `total_votes`,`private`,`expire_date`)	VALUES(
+	$stmt = $pdo->prepare('INSERT INTO `polls` (`id`,  `name`, `total_votes`,`private`,`expire_date`)	VALUES(
 			:id,
 			:name,
 			:total_votes,
 			:private,
 			:expire_date)');
+				 
+			
 		$stmt -> bindValue(':id', $id, PDO::PARAM_INT); 
 		$stmt -> bindValue(':name', $name, PDO::PARAM_STR);
 		$stmt -> bindValue(':total_votes',0 , PDO::PARAM_INT);
 		$stmt -> bindValue(':private', $private, PDO::PARAM_INT);
-		$stmt -> bindValue(':expDate', $expire_date, PDO::PARAM_LOB);
+		$stmt -> bindValue(':expire_date', $expire_date, PDO::PARAM_INT);
 		$stmt -> execute();
 
+	
 	$stmt=$pdo->query('SELECT id FROM options');
 	$oid= $stmt->rowCount()+1;
 
@@ -45,7 +49,7 @@ try
 				:amount)');
 			$stmt -> bindValue(':id', $oid, PDO::PARAM_INT); 
 			$stmt -> bindValue(':poll_id', $id, PDO::PARAM_INT);
-			$stmt -> bindValue(':name',$option_number[$i] , PDO::PARAM_STR);
+			$stmt -> bindValue(':name', $option_number[$i]["name"] , PDO::PARAM_STR);
 			$stmt -> bindValue(':amount', 0, PDO::PARAM_INT);
 			$stmt -> execute();
 		$i++;
