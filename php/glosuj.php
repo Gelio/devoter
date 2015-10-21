8c5ece1b6f1b610c7685e4faa2bd46d8f488c268
Sarkerq <kosma1996@gmail.com>
2015-10-21 22:18:38 +0200

    Voting overhauled, error codes encoded, sql sentences prepared, should work now

--- php/glosuj.php
+++ php/glosuj.php
@@ -2,7 +2,7 @@
 <?php
 	include 'config.inc.php';
 
-	$ip = getenv('HTTP_CLIENT_IP')?:
+	$ip =getenv('HTTP_CLIENT_IP')?:
 	getenv('HTTP_X_FORWARDED_FOR')?:
 	getenv('HTTP_X_FORWARDED')?:
 	getenv('HTTP_FORWARDED_FOR')?:
@@ -14,43 +14,75 @@ if (filter_var($ip, FILTER_VALIDATE_IP))
 	try
    {
 		$pdo = new PDO("mysql:host=".$database['ip'].";
-	  dbname=".$database['db']." ;port=".$database['port'].";
-	  charset=utf8,".$database['user'].",".$database['password'] ,
-	  array(PDO::ATTR_EMULATE_PREPARES => false,
-	  PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+		dbname=".$database['db'].";port=".$database['port'].";
+		encoding=utf8",$database['user'],$database['password'] ,
+		array(PDO::ATTR_EMULATE_PREPARES => false,
+		PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
   		
+		if ($_SERVER['REQUEST_METHOD'] == 'POST' && empty($_POST)) $_POST = json_decode(file_get_contents('php://input'), true);
+		
 		$option =$_POST['optionID'];
 		$poll=$_POST['id'];
-		$stmt= $pdo -> query("SELECT * FROM ip_voted WHERE poll_id=$poll AND IP=$ip");
+	
+		$stmt = $pdo->prepare('SELECT * FROM ip_voted WHERE poll_id=:poll AND IP=:ip');
+			$stmt -> bindValue(':poll', $poll, PDO::PARAM_INT);
+			$stmt -> bindValue(':ip', $ip , PDO::PARAM_STR);
+			$stmt -> execute();
 		$ilosc=$stmt->rowCount();
-		
-		$stmt= $pdo -> query("SELECT * FROM ip_voted");
-		$idv=$stmt->rowCount()+1;
 		
 		if(!$ilosc)
 		{
-			$stmt= $pdo -> exec("insert into ip_voted values($idv,$poll,$ip)");
-			$stmt= $pdo -> exec("update polls set total_votes=total_votes+1 where id=$poll");
-			$stmt =$pdo -> exec("update options set answers=answers+1 where id=$option;")
-			$idv++;
+			//ADDING IP TO VOTED LIST
+			$stmt= $pdo -> prepare("INSERT INTO `ip_voted` (  `poll_id`, `IP`) VALUES(:poll,:ip)");
+			$stmt -> bindValue(':poll', $poll, PDO::PARAM_INT);
+			$stmt -> bindValue(':ip', $ip , PDO::PARAM_STR);
+			$stmt -> execute();
+			//UPDATING POLL SCORE
+			$stmt= $pdo -> prepare("UPDATE polls SET total_votes=total_votes+1 WHERE id=:id");
+			$stmt -> bindValue(':id', $poll, PDO::PARAM_INT);
+			$stmt -> execute();
+			//UPDATING OPTION SCORE
+			$stmt =$pdo -> prepare("UPDATE options SET amount=amount+1 WHERE id=:opt");
+			$stmt -> bindValue(':opt', $option, PDO::PARAM_INT);
+			$stmt -> execute();
+			
+			
+			$erro= array
+			(
+				'error' => 0,
+				'message' => ""
+			);
+			echo json_encode($erro);
 		}
 		else 
 		{
-			echo "You already DeVoted for some option!";
+			$erro= array
+		(
+			'error' => 8,
+			'message' => "You already DeVoted for some option!"
+		);
+		echo json_encode($erro);
 		}
-	}
+   }
+	
 	catch(PDOException $e)
    {
-      echo 'Connection could not be created: ' . $e->getMessage();
-	  echo json_encode($e);
+	    $erro= array
+		(
+			'error' => 1,
+			'message' => 'Connection could not be created: ' . $e->getMessage()
+		);
+		echo json_encode($erro);
    }
 
 }
 else
 {
-	$erro="Your IP couldn't be validated!";
-	
-	echo  $erro;
+	$erro= array
+		(
+			'error' => 0,
+			'message' => "Your IP couldn't be validated!"
+		);
 	echo json_encode($erro);
 	
 }
