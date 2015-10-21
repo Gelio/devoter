d1f4deb7f97f63bc0095d55dfaa25fd6b2353c5e
gvnbleid <mkkrwc@gmail.com>
2015-10-21 20:29:38 +0200

    Fixed bugs in most-popular and single-poll. set limitTo and startFrom in most-popular.php

--- php/most-popular.php
+++ php/most-popular.php
@@ -1,5 +1,15 @@
-<?php
-// TODO: set the limit to be passed in by $_GET parameter (limitTo) as suggested in the API-info
+﻿<?php
+	
+	if (empty($_GET['startFrom']))
+		$startFrom = 0;
+	else
+		$startFrom = $_GET['startFrom'];
+	
+	if (empty($_GET['limitTo']))
+		$limitTo = 3;
+	else
+		$limitTo = $_GET['limitTo'];
+	
 	$ip = $_SERVER['REMOTE_ADDR'];
 	include "config.inc.php";
 	try
@@ -8,44 +18,79 @@
 		$database['user'], $database['password'] );
 		$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 		$stmt = $pdo->query("SELECT id, name, total_votes, private, expire_date FROM polls ORDER BY total_votes DESC 
-		LIMIT 3");
+		LIMIT $startFrom, $limitTo");
 		
 		$wynik = array();
 		while($row = $stmt -> fetch())
 		{
-			$wynik[$row['id']] = array(
-			'id' => $row['id'], 
-			'name' => $row['name'],
-			'options' => array(),
-			'expDate' => $row['expire_date'],
-			'private' => $row['private']
-			);
-		}
+			$number = "SELECT COUNT(*) FROM ip_voted WHERE IP = $ip, poll_id = 'id'";
+			if($number !=0)
+			{
+				
+				$wynik[$row['id']] = array(
+					'id' => $row['id'], 
+					'name' => $row['name'],
+					'options' => array(),
+					'expDate' => $row['expire_date'],
+					'hasVoted' => true
+				);
 			
+			}
+			
+			if($number == 0)
+			{
+				$wynik[$row['id']] = array(
+					'id' => $row['id'], 
+					'name' => $row['name'],
+					'options' => array(),
+					'expDate' => $row['expire_date'],
+					'hasVoted' => false
+				);
+			}
+			
+		}
 		$stmt -> closeCursor();
 		
 		$stmt = $pdo -> query('SELECT id, name, amount, poll_id FROM options ORDER BY poll_id, id');
+		
 		
 		while($row = $stmt -> fetch())
 		{
 			if (!array_key_exists($row['poll_id'], $wynik))
 				continue;
-			$wynik[$row['poll_id']]['options'][] = array(
-				'id' => $row['id'], 
-				'name' => $row['name'],
-				'amount' => $row['amount']
-			);
+			if($number != 0 || $limitTo = 3)
+			{
+				$wynik[$row['poll_id']]['options'][] = array(
+					'id' => $row['id'], 
+					'name' => $row['name'],
+					'amount' => $row['amount']
+				);
+			}
+			else
+			{
+				$wynik[$row['poll_id']]['options'][] = array(
+					'id' => $row['id'], 
+					'name' => $row['name'],
+					'amount' => 0
+				);
+			}
 			
 		}
 		$stmt -> closeCursor();
 		
-		echo json_encode($wynik);
+		$nowy_wynik;
+		$i =0;
+		foreach($wynik as $element)
+		{
+			$nowy_wynik[$i] = $element;
+			$i++;
+		}
 		
+		echo json_encode($nowy_wynik);		
 	}
 	catch(PDOException $e)
 	{
 		echo 'UPS...SOMETHING WENT WRONG :(<br />'.$e; 
 	}
 	?>
-	
 	
--- php/single-poll.php
+++ php/single-poll.php
@@ -1,10 +1,14 @@
 <?php
 	include "config.inc.php";
+	if (empty($_GET['id']))
+			$id = 1;
+		else
+			$id = $_GET['id'];
 	try
 	{
 		$ip = $_SERVER['REMOTE_ADDR'];
 		
-		$id = $_GET['id'];
+		
 		
 		$pdo = new PDO('mysql:host='.$database['ip'].';dbname='.$database['db'].';port='.$database['port'], 
 		$database['user'], $database['password'] );
@@ -12,44 +16,39 @@
 		$stmt = $pdo->query("SELECT id, name, total_votes, private, expire_date FROM polls WHERE id = $id");
 		$wynik = array();
 		$number = "SELECT COUNT(*) FROM ip_voted WHERE IP = $ip, poll_id = $id";
-		while($row = $stmt -> fetch())
+		$row = $stmt -> fetch();
+		if($number !=0)
 		{
-			if($number !=0)
-			{
-				$wynik[$row['id']] = array(
-			'id' => $row['id'], 
-			'name' => $row['name'],
-			'options' => array(),
-			'expDate' => $row['expire_date'],
-			'private' => $row['private'],
-			'hasVoted' => true
-			);
-			}
-			
-			if($number == 0)
-			{
-				$wynik[$row['id']] = array(
-			'id' => $row['id'], 
-			'name' => $row['name'],
-			'options' => array(),
-			'expDate' => $row['expire_date'],
-			'private' => $row['private'],
-			'hasVoted' => false
-			);
-			}
-			
+			$wynik = array(
+				'id' => $row['id'], 
+				'name' => $row['name'],
+				'options' => array(),
+				'expDate' => $row['expire_date'],
+				'private' => $row['private'],
+				'hasVoted' => true
+				);
+		}
+		
+		if($number == 0)
+		{
+			$wynik = array(
+				'id' => $row['id'], 
+				'name' => $row['name'],
+				'options' => array(),
+				'expDate' => $row['expire_date'],
+				'private' => $row['private'],
+				'hasVoted' => false
+				);
 		}
 		$stmt -> closeCursor();
 		
-		$stmt = $pdo -> query("SELECT id, name, amount, poll_id FROM options ORDER BY id" );
+		$stmt = $pdo -> query("SELECT id, name, amount, poll_id FROM options WHERE poll_id = $id ORDER BY id " );
 		
 		while($row = $stmt -> fetch())
 		{
-			if (!array_key_exists($row['poll_id'], $wynik))
-				continue;
 			if($number != 0)
 			{
-				$wynik[$row['poll_id']]['options'][] = array(
+				$wynik['options'][] = array(
 					'id' => $row['id'], 
 					'name' => $row['name'],
 					'amount' => $row['amount']
@@ -57,7 +56,7 @@
 			}
 			else
 			{
-				$wynik[$row['poll_id']]['options'][] = array(
+				$wynik['options'][] = array(
 					'id' => $row['id'], 
 					'name' => $row['name'],
 					'amount' => 0
@@ -68,7 +67,6 @@
 		$stmt -> closeCursor();
 		
 		echo json_encode($wynik);
-		
 	}
 	
 	catch(PDOException $e)
